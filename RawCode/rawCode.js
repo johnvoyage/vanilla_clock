c1288b2d2f79734363720f1de29aa1c77111be31
johnvoyage <jmyork762@gmail.com>
2018-05-18 14:55:17 -0400

    15 works

--- RawCode/rawCode.js
+++ RawCode/rawCode.js
@@ -1,3 +1,5 @@
+let framesPerSecond = 1000;
+
 const ballPositions = {
   queueTrack: [],
   minuteTrack: [],
@@ -5,7 +7,40 @@ const ballPositions = {
   hourTrack: []
 };
 
-const originalPositions = {};
+let originalPositions = {
+  queueTrack: [],
+  minuteTrack: [],
+  fiveMinuteTrack: [],
+  hourTrack: []
+};
+
+// let originalPositions = {
+//   queueTrack: [
+//     11,
+//     5,
+//     26,
+//     18,
+//     2,
+//     30,
+//     19,
+//     8,
+//     24,
+//     10,
+//     29,
+//     20,
+//     16,
+//     21,
+//     28,
+//     1,
+//     23,
+//     14,
+//     27,
+//     9
+//   ],
+//   minuteTrack: [],
+//   fiveMinuteTrack: [22, 13, 25, 3, 7],
+//   hourTrack: [6, 12, 17, 4, 15]
+// };
 
 let morning = true;
 let daysPassed = 0;
@@ -20,11 +55,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const hourDiv = document.getElementById("hourtrack");
   const queueDiv = document.getElementById("queuetrack");
 
+  // const initialOrderTest = () => {
+  //   // debugger;
+  //   for (let track in ballPositions) {
+  //     let counter = 0;
+  //     for (let ball of ballPositions[track]) {
+  //       if (daysPassed === 15) {
+  //         console.log(originalPositions[track][counter]);
+  //         console.log(ball);
+  //       }
+  //
+  //       if (originalPositions[track][counter] !== ball) {
+  //         return false;
+  //       } else {
+  //         counter++;
+  //       }
+  //     }
+  //   }
+  //   return true;
+  // };
+
   const ballClock = (numberOfBalls, numberOfMinutes = null) => {
     // ballPositions.hourTrack.push(1);
     minutesRemaining = numberOfMinutes;
     putBallsOnQueueTrack(numberOfBalls);
-    assignOriginalPositions(ballPositions);
+    assignOriginalPositions(numberOfBalls);
+    // console.log(ballPositions);
+    // console.log(originalPositions);
+    // console.log(initialOrderTest());
+    // debugger;
     showTracks();
     startClock();
   };
@@ -35,9 +94,9 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const assignOriginalPositions = ballPositions => {
-    for (let key in ballPositions) {
-      originalPositions[key] = ballPositions[key];
+  const assignOriginalPositions = number => {
+    for (let counter = 1; counter <= number; counter++) {
+      originalPositions.queueTrack.push(counter);
     }
   };
 
@@ -97,12 +156,15 @@ document.addEventListener("DOMContentLoaded", () => {
       // console.log("test2: ", minutesRemaining - minutesPassed === 0);
 
       if (keepGoingOrNot()) {
+        // console.log(ballPositions);
+
         moveNextBallInQueue();
         minutesPassed += 1;
         showTracks();
       }
       // }
-    }, 1);
+      // console.log(initialOrderTest());
+    }, 1000 / framesPerSecond);
   };
 
   const keepGoingOrNot = () => {
@@ -111,22 +173,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const initialOrderTest = () => {
     // debugger;
-    for (let track in ballPositions) {
-      let counter = 0;
-      for (let ball of ballPositions[track]) {
-        if (daysPassed === 15) {
-          console.log(originalPositions[track][counter]);
-          console.log(ball);
-        }
-
-        if (originalPositions[track][counter] !== ball) {
-          return false;
-        } else {
-          counter++;
-        }
-      }
+    if (minutesPassed < 1 || minutesRemaining !== null) {
+      // console.log("here");
+      return true;
     }
-    return true;
+
+    if (
+      ballPositions.queueTrack.length === originalPositions.queueTrack.length
+    ) {
+      let counter = 0;
+      while (counter < ballPositions.queueTrack.length) {
+        if (
+          ballPositions.queueTrack[counter] !==
+          originalPositions.queueTrack[counter]
+        ) {
+          return true;
+        }
+        counter++;
+      } // console.log(ballPositions.queueTrack);
+      // console.log(originalPositions.queueTrack);
+      return false;
+    } else {
+      return true;
+    }
+
+    // for (let track in ballPositions) {
+    //   let counter = 0;
+    //   for (let ball of ballPositions[track]) {
+    //     // if (daysPassed === 15) {
+    //     //   console.log(originalPositions[track][counter]);
+    //     //   console.log(ball);
+    //     // console.log(originalPositions);
+    //     // console.log("orig: ", originalPositions[track][counter]);
+    //     // console.log("ballPositions: ", ball);
+    //
+    //     if (originalPositions[track][counter] !== ball) {
+    //       return true;
+    //     } else {
+    //       counter++;
+    //     }
+    //   }
+    // }
+    // return false;
   };
 
   const numberOfMinutesTest = () => {
@@ -148,7 +236,8 @@ document.addEventListener("DOMContentLoaded", () => {
         clearTrackCheck("fiveMinuteTrack", 11)
           ? (clearTrack(ballPositions.fiveMinuteTrack),
             clearTrackCheck("hourTrack", 11)
-              ? clearTrack(ballPositions.hourTrack)
+              ? (clearTrack(ballPositions.hourTrack),
+                ballPositions.queueTrack.push(ball))
               : ballPositions.hourTrack.push(ball))
           : ballPositions.fiveMinuteTrack.push(ball))
       : ballPositions.minuteTrack.push(ball);
@@ -192,8 +281,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ballPositions.queueTrack.push(ball);
   };
 
-  ballClock(30); // "30 balls cycle after 15 days"
-  // ballClock(45) // "45 balls cycle after 378 days"
+  // ballClock(30); // "30 balls cycle after 15 days"
+  ballClock(45); // "45 balls cycle after 378 days"
   // ballClock(30, 325);
   /*
     {
