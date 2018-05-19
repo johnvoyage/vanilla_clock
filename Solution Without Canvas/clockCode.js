8839db5f22a47d275ae4fc825db6c0a5b0a1419b
johnvoyage <jmyork762@gmail.com>
2018-05-19 12:24:30 -0400

    working...

--- Solution Without Canvas/clockCode.js
+++ Solution Without Canvas/clockCode.js
@@ -1,27 +1,30 @@
-let framesPerSecond = 1000;
+// let framesPerSecond = 1000;
 
-const letItRun = () => {
-  return false;
-};
+// let framesPerSecond;
 
-const ballPositions = {
-  queueTrack: [],
-  minuteTrack: [],
-  fiveMinuteTrack: [],
-  hourTrack: []
-};
-
-const originalPositions = {
-  queueTrack: [],
-  minuteTrack: [],
-  fiveMinuteTrack: [],
-  hourTrack: []
-};
-
-let morning = true;
-let daysPassed = 0;
-let minutesPassed = 0;
-let minutesRemaining;
+// const letItRun = () => {
+//   return false;
+// };
+// let isPaused = false;
+//
+// let ballPositions = {
+//   queueTrack: [],
+//   minuteTrack: [],
+//   fiveMinuteTrack: [],
+//   hourTrack: []
+// };
+//
+// let originalPositions = {
+//   queueTrack: [],
+//   minuteTrack: [],
+//   fiveMinuteTrack: [],
+//   hourTrack: []
+// };
+//
+// let morning = true;
+// let daysPassed = 0;
+// let minutesPassed = 0;
+// let minutesRemaining;
 
 // const setTrack = (track, number) => {
 //   for (let counter = 1; counter <= number; counter++) {
@@ -29,179 +32,182 @@ let minutesRemaining;
 //   }
 // };
 
-const clockStarted = () => {
-  // document.addEventListener("DOMContentLoaded", () => {
-  // const boardDiv = document.getElementById("board");
-  // const minDiv = document.getElementById("mintrack");
-  // const fiveMinDiv = document.getElementById("fivemintrack");
-  // const hourDiv = document.getElementById("hourtrack");
-  // const queueDiv = document.getElementById("queuetrack");
-  // const timeHeader = document.getElementById("time");
-  // const daysHeader = document.getElementById("days");
-  // const minPassedHeader = document.getElementById("minpassed");
-  // const minRemainingHeader = document.getElementById("minremain");
+// const clockStarted = (numberOfBalls, numberOfMinutes) => {
+// document.addEventListener("DOMContentLoaded", () => {
+// const boardDiv = document.getElementById("board");
+// const minDiv = document.getElementById("mintrack");
+// const fiveMinDiv = document.getElementById("fivemintrack");
+// const hourDiv = document.getElementById("hourtrack");
+// const queueDiv = document.getElementById("queuetrack");
+// const timeHeader = document.getElementById("time");
+// const daysHeader = document.getElementById("days");
+// const minPassedHeader = document.getElementById("minpassed");
+// const minRemainingHeader = document.getElementById("minremain");
 
-  const ballClock = (numberOfBalls, numberOfMinutes = null) => {
-    minutesRemaining = numberOfMinutes;
-    setTrack(ballPositions, numberOfBalls);
-    setTrack(originalPositions, numberOfBalls);
-    showInfo();
-    startClock();
-  };
+const clockStarted = (numberOfBalls, numberOfMinutes) => {
+  // console.log(numberOfMinutes);
+  minutesRemaining = numberOfMinutes;
+  setTrack(ballPositions, numberOfBalls);
+  setTrack(originalPositions, numberOfBalls);
+  showInfo();
+  startClock();
+};
 
-  const setTrack = (track, number) => {
-    for (let counter = 1; counter <= number; counter++) {
-      track.queueTrack.push(counter);
-    }
-  };
+const setTrack = (track, number) => {
+  for (let counter = 1; counter <= number; counter++) {
+    track.queueTrack.push(counter);
+  }
+};
 
-  const showInfo = () => {
-    timeHeader.innerText = `Current time: ${hourAndMinutes()} ${amOrPm()}`;
-    daysHeader.innerText = `Total days passed: ${formatWithCommas(
-      dayFormat()
-    )}`;
-    minPassedHeader.innerText = `Total minutes passed: ${formatWithCommas(
-      minutesPassed
-    )}`;
-    minRemainingHeader.innerText = `Total minutes remaining: ${formatMinutesRemaining()}`;
-    minDiv.innerText = `Balls: ${ballPositions.minuteTrack}`;
-    fiveMinDiv.innerText = `Balls: ${ballPositions.fiveMinuteTrack}`;
-    hourDiv.innerText = `Balls: ${ballPositions.hourTrack}`;
-    queueDiv.innerText = `Balls: ${ballPositions.queueTrack}`;
-  };
+const showInfo = () => {
+  timeHeader.innerText = `Current time: ${hourAndMinutes()} ${amOrPm()}`;
+  daysHeader.innerText = `Total days passed: ${formatWithCommas(dayFormat())}`;
+  minPassedHeader.innerText = `Total minutes passed: ${formatWithCommas(
+    minutesPassed
+  )}`;
+  minRemainingHeader.innerText = `Total minutes remaining: ${formatMinutesRemaining()}`;
+  minDiv.innerText = `Balls: ${ballPositions.minuteTrack}`;
+  fiveMinDiv.innerText = `Balls: ${ballPositions.fiveMinuteTrack}`;
+  hourDiv.innerText = `Balls: ${ballPositions.hourTrack}`;
+  queueDiv.innerText = `Balls: ${ballPositions.queueTrack}`;
+};
 
-  const hourAndMinutes = () => {
-    return `${timeFormat(ballPositions.hourTrack.length + 1)}:${timeFormat(
-      ballPositions.fiveMinuteTrack.length * 5 +
-        ballPositions.minuteTrack.length
-    )}`;
-  };
+const hourAndMinutes = () => {
+  return `${timeFormat(ballPositions.hourTrack.length + 1)}:${timeFormat(
+    ballPositions.fiveMinuteTrack.length * 5 + ballPositions.minuteTrack.length
+  )}`;
+};
 
-  const amOrPm = () => {
-    hourAndMinutes() === "12:00"
-      ? ((morning = !morning), (daysPassed += 0.5))
-      : null;
-    return morning ? "AM" : "PM";
-  };
+const amOrPm = () => {
+  hourAndMinutes() === "12:00"
+    ? ((morning = !morning), (daysPassed += 0.5))
+    : null;
+  return morning ? "AM" : "PM";
+};
 
-  const timeFormat = number => {
-    return number >= 10 ? number : "0" + number;
-  };
+const timeFormat = number => {
+  return number >= 10 ? number : "0" + number;
+};
 
-  const dayFormat = () => {
-    return Math.floor(daysPassed);
-  };
+const dayFormat = () => {
+  return Math.floor(daysPassed);
+};
 
-  const formatWithCommas = number => {
-    return number
-      .toString()
-      .split("")
-      .reverse()
-      .join("")
-      .match(/.{1,3}/g)
-      .join(",")
-      .split("")
-      .reverse()
-      .join("");
-  };
+const formatWithCommas = number => {
+  return number
+    .toString()
+    .split("")
+    .reverse()
+    .join("")
+    .match(/.{1,3}/g)
+    .join(",")
+    .split("")
+    .reverse()
+    .join("");
+};
 
-  const formatMinutesRemaining = () => {
-    return minutesRemaining === null
-      ? "N/A"
-      : formatWithCommas(minutesRemaining - minutesPassed);
-  };
+const formatMinutesRemaining = () => {
+  return minutesRemaining === null
+    ? "N/A"
+    : formatWithCommas(minutesRemaining - minutesPassed);
+};
 
-  const startClock = () => {
-    setInterval(() => {
+const startClock = () => {
+  interval += 1;
+  setInterval(() => {
+    if (!isPaused) {
       if (keepGoingOrNot()) {
         moveNextBallInQueue();
         minutesPassed += 1;
         showInfo();
+      } else {
+        clockCycleDone();
       }
-    }, 1000 / framesPerSecond);
-  };
-
-  const keepGoingOrNot = () => {
-    return (initialOrderTest() && numberOfMinutesTest()) || letItRun();
-  };
-
-  const initialOrderTest = () => {
-    if (minutesPassed < 1 || minutesRemaining !== null) {
-      return true;
     }
+    // console.log(framesPerSecond);
+    console.log(Math.floor(1000 / framesPerSecond));
+  }, 1000 / framesPerSecond);
+};
 
-    if (
-      ballPositions.queueTrack.length === originalPositions.queueTrack.length
-    ) {
-      let counter = 0;
-      while (counter < ballPositions.queueTrack.length) {
-        if (
-          ballPositions.queueTrack[counter] !==
-          originalPositions.queueTrack[counter]
-        ) {
-          return true;
-        }
-        counter++;
+const keepGoingOrNot = () => {
+  return initialOrderTest() && numberOfMinutesTest();
+};
+
+const initialOrderTest = () => {
+  if (minutesPassed < 1 || minutesRemaining !== null) {
+    return true;
+  }
+
+  if (ballPositions.queueTrack.length === originalPositions.queueTrack.length) {
+    let counter = 0;
+    while (counter < ballPositions.queueTrack.length) {
+      if (
+        ballPositions.queueTrack[counter] !==
+        originalPositions.queueTrack[counter]
+      ) {
+        return true;
       }
-      return false;
-    } else {
-      return true;
+      counter++;
     }
-  };
+    return false;
+  } else {
+    return true;
+  }
+};
 
-  const numberOfMinutesTest = () => {
-    return minutesRemaining === null || minutesRemaining - minutesPassed !== 0;
-  };
+const numberOfMinutesTest = () => {
+  return minutesRemaining === null || minutesRemaining - minutesPassed !== 0;
+};
 
-  const moveNextBallInQueue = () => {
-    moveFromQueueToMinute(ballPositions.queueTrack.shift());
-  };
+const moveNextBallInQueue = () => {
+  moveFromQueueToMinute(ballPositions.queueTrack.shift());
+};
 
-  const moveFromQueueToMinute = ball => {
-    clearTrackCheck("minuteTrack", 4)
-      ? (clearTrack(ballPositions.minuteTrack),
-        clearTrackCheck("fiveMinuteTrack", 11)
-          ? (clearTrack(ballPositions.fiveMinuteTrack),
-            clearTrackCheck("hourTrack", 11)
-              ? (clearTrack(ballPositions.hourTrack),
-                ballPositions.queueTrack.push(ball))
-              : ballPositions.hourTrack.push(ball))
-          : ballPositions.fiveMinuteTrack.push(ball))
-      : ballPositions.minuteTrack.push(ball);
-  };
+const moveFromQueueToMinute = ball => {
+  clearTrackCheck("minuteTrack", 4)
+    ? (clearTrack(ballPositions.minuteTrack),
+      clearTrackCheck("fiveMinuteTrack", 11)
+        ? (clearTrack(ballPositions.fiveMinuteTrack),
+          clearTrackCheck("hourTrack", 11)
+            ? (clearTrack(ballPositions.hourTrack),
+              ballPositions.queueTrack.push(ball))
+            : ballPositions.hourTrack.push(ball))
+        : ballPositions.fiveMinuteTrack.push(ball))
+    : ballPositions.minuteTrack.push(ball);
+};
 
-  const clearTrackCheck = (track, maxLength) => {
-    return ballPositions[track].length === maxLength;
-  };
+const clearTrackCheck = (track, maxLength) => {
+  return ballPositions[track].length === maxLength;
+};
 
-  const clearTrack = track => {
-    const trackLength = track.length;
-    for (let i = 0; i < trackLength; i++) {
-      ballPositions.queueTrack.push(track.pop());
-    }
-  };
+const clearTrack = track => {
+  const trackLength = track.length;
+  for (let i = 0; i < trackLength; i++) {
+    ballPositions.queueTrack.push(track.pop());
+  }
+};
 
-  const moveFromMinuteToFiveMinute = ball => {
-    clearTrackCheck("fiveMinuteTrack", 9)
-      ? clearTrack(ballPositions.fiveMinuteTrack)
-      : ballPositions.fiveMinuteTrack.push(ball);
-  };
+const moveFromMinuteToFiveMinute = ball => {
+  clearTrackCheck("fiveMinuteTrack", 9)
+    ? clearTrack(ballPositions.fiveMinuteTrack)
+    : ballPositions.fiveMinuteTrack.push(ball);
+};
 
-  const moveFromFiveMinuteToHour = ball => {
-    clearTrackCheck("hourTrack", 11)
-      ? clearTrack(ballPositions.hourTrack)
-      : ballPositions.hourTrack.push(ball);
-  };
+const moveFromFiveMinuteToHour = ball => {
+  clearTrackCheck("hourTrack", 11)
+    ? clearTrack(ballPositions.hourTrack)
+    : ballPositions.hourTrack.push(ball);
+};
 
-  const moveFromHourToQueue = ball => {
-    ballPositions.queueTrack.push(ball);
-  };
+const moveFromHourToQueue = ball => {
+  ballPositions.queueTrack.push(ball);
+};
 
-  // ballClock(27);
-  // ballClock(30); // "30 balls cycle after 15 days"
-  // ballClock(45); // "45 balls cycle after 378 days"
-  ballClock(30, 325);
-  /*
+// clockStarted(27);
+// clockStarted(30); // "30 balls cycle after 15 days"
+// clockStarted(45); // "45 balls cycle after 378 days"
+// clockStarted(30, 325);
+/*
     {
       "Min":[],
       "FiveMin":[22,13,25,3,7],
@@ -209,4 +215,4 @@ const clockStarted = () => {
       "Main": [11,5,26,18,2,30,19,8,24,10,29,20,16,21,28,1,23,14,27,9]
     }
   */
-};
+// };
--- Solution Without Canvas/userInput.js
+++ Solution Without Canvas/userInput.js
@@ -1,12 +1,44 @@
 let boardDiv;
-let minDivk;
-let fiveMinDivtrack;
-let hourDivck;
-let queueDivack;
+let minDiv;
+let fiveMinDiv;
+let hourDiv;
+let queueDiv;
+let initialQueueDiv;
 let timeHeader;
 let daysHeader;
-let minPassedHeadered;
-let minRemainingHeaderin;
+let minPassedHeader;
+let minRemainingHeader;
+
+// let minutesRemaining;
+let framesPerSecond;
+let interval = 0;
+
+let isPaused;
+// = false;
+
+let ballPositions;
+// = {
+//   queueTrack: [],
+//   minuteTrack: [],
+//   fiveMinuteTrack: [],
+//   hourTrack: []
+// };
+
+let originalPositions;
+//  = {
+//   queueTrack: [],
+//   minuteTrack: [],
+//   fiveMinuteTrack: [],
+//   hourTrack: []
+// };
+
+let morning;
+// = true;
+let daysPassed;
+// = 0;
+let minutesPassed;
+// = 0;
+
 const initialQueue = [];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -15,6 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
   fiveMinDiv = document.getElementById("fivemintrack");
   hourDiv = document.getElementById("hourtrack");
   queueDiv = document.getElementById("queuetrack");
+  initialQueueDiv = document.getElementById("initialqueue");
   timeHeader = document.getElementById("time");
   daysHeader = document.getElementById("days");
   minPassedHeader = document.getElementById("minpassed");
@@ -23,36 +56,140 @@ document.addEventListener("DOMContentLoaded", () => {
   const ballForm = document.createElement("form");
   ballForm.innerHTML = `
     <label>Number of balls (27 - 127)</label>
-    <input id='ballInput' required type='number' min='27' max='127'>
+    <input id='ballinput' required value='30' type='number' min='27' max='127'>
     <br>
     <label>Number of minutes (optional)</label>
-    <input id='minuteInput' min='1', step='1' type='number'>
+    <input id='minuteinput' value='325' min='1', step='1' type='number'>
     <br>
     <label>Speed in FPS (1-1,000)</label>
-    <input type="range" min="1" max="1000" value="1000">
+    <select id="speedinput">
+      <option>1000</option>
+      <option>100</option>
+      <option>10</option>
+      <option>1</option>
+    </select>
     <br>
-    <input type='submit'>
+    <input id='runbutton' type='submit' value='run'>
+    <input id='resetbutton' type='button' value='reset' disabled>
+    <input id='pauseplaybutton' type='button' value='pause' disabled>
+
   `;
   formDiv = document.getElementById("formdiv");
   ballForm.addEventListener("submit", handleFormSubmit);
   formDiv.append(ballForm);
-  const ballNumberInput = document.getElementById("ballInput");
+  const ballNumberInput = document.getElementById("ballinput");
   ballNumberInput.addEventListener("change", handleBallChange);
+  const resetButton = document.getElementById("resetbutton");
+  resetButton.addEventListener("click", handleReset);
+  const pausePlayButton = document.getElementById("pauseplaybutton");
+  pausePlayButton.addEventListener("click", handlePausePlay);
 });
 
+const handleReset = event => {
+  console.log("reset");
+  clearInterval(interval);
+  resetForm();
+  resetVariables();
+  resetScreen();
+};
+
+const handlePausePlay = event => {
+  const newValue = !isPaused ? "play" : "pause";
+  document.getElementById("pauseplaybutton").setAttribute("value", newValue);
+  isPaused = !isPaused;
+};
+
 const handleFormSubmit = event => {
+  resetVariables();
   event.preventDefault();
-  console.log(event);
-  event.target.reset();
+  // debugger;
+
+  const numberOfBallsInput = parseInt(event.target.children[1].value);
+  const minutesRemainingInput =
+    event.target.children[4].value === ""
+      ? null
+      : event.target.children[4].value;
+  // console.log(event);
+  framesPerSecond = parseInt(event.target.children[7].value);
+  // console.log(framesPerSecond);
+  clockStarted(numberOfBallsInput, minutesRemainingInput);
+  // event.target.reset();
+  // event.target.remove();
+  disableForm();
+};
+
+const clockCycleDone = () => {
+  clearInterval(interval);
+  // showInfo();
+  document.getElementById("pauseplaybutton").setAttribute("disabled", true);
+
+  // resetVariables();
+  // enableForm();
+};
+
+const resetVariables = () => {
+  days = 0;
+  minutesRemaining = 0;
+  morning = true;
+  daysPassed = 0;
+  minutesPassed = 0;
+  originalPositions = {
+    queueTrack: [],
+    minuteTrack: [],
+    fiveMinuteTrack: [],
+    hourTrack: []
+  };
+  ballPositions = {
+    queueTrack: [],
+    minuteTrack: [],
+    fiveMinuteTrack: [],
+    hourTrack: []
+  };
+  isPaused = false;
+};
+
+const resetScreen = () => {
+  timeHeader.innerText = `Current time: ${hourAndMinutes()} ${amOrPm()}`;
+  daysHeader.innerText = `Total days passed: ${formatWithCommas(dayFormat())}`;
+  minPassedHeader.innerText = `Total minutes passed: ${formatWithCommas(
+    minutesPassed
+  )}`;
+  minRemainingHeader.innerText = `Total minutes remaining: ${formatMinutesRemaining()}`;
+  minDiv.innerText = `Balls: ${ballPositions.minuteTrack}`;
+  fiveMinDiv.innerText = `Balls: ${ballPositions.fiveMinuteTrack}`;
+  hourDiv.innerText = `Balls: ${ballPositions.hourTrack}`;
+  queueDiv.innerText = `Balls: ${ballPositions.queueTrack}`;
+  setInitialQueue(30);
+  initialQueueDiv.innerText = `Initial queue: ${initialQueue}`;
+};
+
+const disableForm = () => {
+  document.getElementById("ballinput").setAttribute("disabled", true);
+  document.getElementById("ballinput").setAttribute("value", "");
+  document.getElementById("minuteinput").setAttribute("disabled", true);
+  document.getElementById("minuteinput").setAttribute("value", "");
+  document.getElementById("speedinput").setAttribute("disabled", true);
+  document.getElementById("runbutton").setAttribute("disabled", true);
+  document.getElementById("resetbutton").removeAttribute("disabled");
+  document.getElementById("pauseplaybutton").removeAttribute("disabled");
+};
+
+const resetForm = () => {
+  document.getElementById("ballinput").removeAttribute("disabled");
+  document.getElementById("ballinput").setAttribute("value", "30");
+  document.getElementById("minuteinput").removeAttribute("disabled");
+  document.getElementById("minuteinput").setAttribute("value", "325");
+  document.getElementById("speedinput").removeAttribute("disabled");
+  document.getElementById("speedinput").setAttribute("value", "1000");
+  document.getElementById("runbutton").removeAttribute("disabled");
+  document.getElementById("resetbutton").setAttribute("disabled", true);
+  document.getElementById("pauseplaybutton").setAttribute("disabled", true);
 };
 
 const handleBallChange = event => {
   const userInput = parseInt(event.target.value);
   setInitialQueue(userInput);
-  queueDiv.innerText = `Initial balls: ${initialQueue}`;
-  // debugger;
-  // console.log(event);
-  // setTrack(ballPositions, numberOfBalls);
+  initialQueueDiv.innerText = `Initial queue: ${initialQueue}`;
 };
 
 const setInitialQueue = number => {
